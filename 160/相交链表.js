adf870e1a8d781da8b40e53479ebded4f36b8140
lixf <1304383417@qq.com>
2022-06-22 14:29:47 +0800

    相交链表

---
+++ 160/相交链表.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @description 集合
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  const visited = new Set();
+  let temp = headA;
+  while (temp !== null) {
+    visited.add(temp);
+    temp = temp.next;
+  }
+  temp = headB;
+  while (temp !== null) {
+    if (visited.has(temp)) {
+      return temp;
+    }
+    temp = temp.next;
+  }
+  return null;
+};
+
+/**
+ * @description 双指针
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  let p1 = headA;
+  let p2 = headB;
+  while (p1 !== p2) {
+    if (p1 === null) p1 = headB;
+    else p1 = p1.next;
+    if (p2 === null) p2 = headA;
+    else p2 = p2.next;
+  }
+  return p1;
+};
