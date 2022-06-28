53c5884008bcb68647995b32574448444a9702fa
lixf <1304383417@qq.com>
2022-06-28 10:22:41 +0800

    航班预定统计

---
+++ 差分数组工具类.js
@@ -0,0 +1,28 @@
+class Difference {
+  constructor(nums) {
+    this.diff = new Array(nums.length);
+    this.diff[0] = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+      this.diff[i] = nums[i] - nums[i - 1];
+    }
+  }
+
+  increment(i, j, val) {
+    this.diff[i] += val;
+    if (j + 1 < this.diff.length) {
+      this.diff[j + 1] -= val;
+    }
+  }
+
+  result() {
+    const res = new Array(this.diff.length);
+    res[0] = this.diff[0];
+    for (let i = 1; i < this.diff.length; i++) {
+      res[i] = res[i - 1] + this.diff[i];
+    }
+    return res;
+  }
+}
+
+const res = new Difference([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+res.increment(3, 6, 10);
