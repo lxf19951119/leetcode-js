f91ad0f0119ed5923b4dd6b6d71d5ef1212a7ad8
lixf <1304383417@qq.com>
2022-08-11 17:18:53 +0800

    丢失的数字

---
+++ 268/丢失的数字.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+  nums.sort((a, b) => a - b);
+  const n = nums.length;
+  for (let i = 0; i < n; i++) {
+    if (nums[i] !== i) return i;
+  }
+  return n;
+};
+
+missingNumber([0, 5, 1, 2, 3]);
