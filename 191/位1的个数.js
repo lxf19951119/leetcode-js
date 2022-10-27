ee1b5e00aac9b6a9356c29909e73ddaa087f13c4
lixf <1304383417@qq.com>
2022-10-27 16:33:03 +0800

    位1的个数

---
+++ 191/位1的个数.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+	debugger;
+	let ret = 0;
+    for (let i = 0; i < 32; i++) {
+        if ((n & (1 << i)) !== 0) {
+            ret++;
+        }
+    }
+    return ret;
+};
